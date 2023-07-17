1032767ea671f9769157bdafd1ef6dc3841b0bee
yoviefp <yoviefp@gmail.com>
2023-07-17 19:08:42 +0700

    Revert "initial commit"
    This reverts commit 885d65060c0f5a01192478899f30f88321dcc2eb.

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,3 +1,1 @@
 export const primaryColor = "rgb(29, 64, 128)";
-
-export const baseUrl = process.env.NEXT_PUBLIC_API_GATEWAY;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,14 @@
+// 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
+
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
-import { QueryClient, QueryClientProvider } from "react-query";
 
-import withRedux from "next-redux-wrapper";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "@/redux/store";
-
-function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-  const config = {
-    initialColorMode: "light",
-    //  If true, your app will change color mode based on the user's system preferences.
-    useSystemColorMode: false,
-  };
-
-  // 3. extend the theme
-  const theme = extendTheme({ config });
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PersistGate loading={null} persistor={persistor}>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </QueryClientProvider>
-    </PersistGate>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
-
-const makeStore = () => store;
-export default withRedux(makeStore)(App);
